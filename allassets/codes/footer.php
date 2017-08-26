c19e72fd798a17af939e56b515ffa555b9fcc4a1
Hášíl Páůďýál <ha5il@users.noreply.github.com>
2017-08-26 13:44:52 +0530

    first commit

---
+++ allassets/codes/footer.php
@@ -0,0 +1,32 @@
+      <footer class="page-footer orange">
+         <div class="container">
+            <div class="row">
+               <div class="col l6 s12">
+                  <h5 class="white-text">Company Bio</h5>
+                  <p class="grey-text text-lighten-4">We are recognised also as Virtuoso Trade and Services PVT Ltd, officed at RMZ Infinity and UB City, Bangalore, India. We are Indian partners to Danube International, Electrolux and Pony</p>
+               </div>
+               <div class="col l3 s12">
+                  <h5 class="white-text">Products</h5>
+                  <ul>
+                     <li><a class="white-text" href="#!">Washer Extractor</a></li>
+                     <li><a class="white-text" href="#!">Tumble Dryer</a></li>
+                     <li><a class="white-text" href="#!">Steam Ironing Table</a></li>
+                     <li><a class="white-text" href="#!">Calendering Machine</a></li>
+                     <li><a class="white-text" href="#!">Spotting Table</a></li>
+                  </ul>
+               </div>
+               <div class="col l3 s12">
+                  <h5 class="white-text">Connect</h5>
+                  <ul>
+                     <li><a class="white-text" href="#!">Purchase</a></li>
+                     <li><a class="white-text" href="#!">Service</a></li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+         <div class="footer-copyright">
+            <div class="container">
+               Request a Call Back</a>
+            </div>
+         </div>
+      </footer>
---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link rel="stylesheet" href="/allassets/codes/materialize.min.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>A2Z Laundry Solutions</title>
+   </head>
+   <body>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/allassets/codes/nav.php');?>
+      <div class="section no-pad-bot" id="index-banner">
+         <div class="container">
+            <br><br>
+            <h1 class="header center orange-text">A2Z Laundry Solutions</h1>
+            <div class="row center">
+               <h5 class="header col s12 light">Professional Solution to Professional Wash</h5>
+            </div>
+            <div class="container">
+               <div class="section">
+                  <div class="row">
+                     <div class="col s12 m6">
+                        <div class="icon-block">
+  <img class="materialboxed" data-caption="DANUBE Washer Extractor" width="100%" src="/allassets/Washer.jpg">
+                           <h5 class="center orange-text">Washer Extractor</h5>
+                           <p class="light">With Standard WET CLEANING feature</p>
+                        </div>
+                     </div>
+                     <div class="col s12 m6">
+                        <div class="icon-block">
+  <img class="materialboxed" data-caption="DANUBE Tumble Dryer" width="100%" src="/allassets/Tumble_Dryer.jpg">
+                           <h5 class="center orange-text">Tumble Dryer</h5>
+                           <p class="light">Wrincle free Drying to Clothes.</p>
+                        </div>
+                     </div>
+                     <div class="col s12 m12">
+                        <div class="icon-block">
+  <img class="materialboxed" data-caption="DANUBE Calendering Machine" width="100%" src="/allassets/Calender.jpg">
+                           <h5 class="center orange-text">Calendering Machine</h5>
+                           <p class="light">Calendering Machines.</p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <br><br>
+			                  <div class="section">
+							  <h5 class="orange-text">Mission</h5>
+			       <p class="flow-text">
+Provide laundry technology to achieve professional quality control in washing, drying, ironing and
+finishing.</p>
+							  <h5 class="orange-text">Vision</h5>
+<p class="flow-text">Help to setup and grow the laundry business.</p>
+
+<h5 class="orange-text">What we do?</h5>
+<p class="flow-text">
+1. Inspection of Site before sales to warn about any future consequences and resolve it.<br>
+2. Change features of machines as per requirement while placing order. Features like selfservice,
+computer integration, professional washing accessories interface, remote
+control…<br>
+3. Installation of Machine and its Service<br>
+4. Teaching the staff to operate machine and resolve any issue they face while working with
+it.<br>
+5. Consulting about further steps to be taken for growth of business.</p>
+</div>
+            </div>
+         </div>
+      </div>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/allassets/codes/footer.php');?>
+      <script type="text/javascript" src="/allassets/codes/jquery-3.2.1.min.js"></script>
+      <script src="/allassets/codes/materialize.min.js"></script>
+	  <script type="text/javascript" >(function($){$(function(){$('.button-collapse').sideNav();});})(jQuery);</script>
+   </body>
+</html>
